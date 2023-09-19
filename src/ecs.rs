47ed9b85360bad3ad57d76be88c047b8fc0706f8
pgurtner <81441242+pgurtner@users.noreply.github.com>
2023-09-19 10:42:54 +0200

    finalizing first ecs draft including simple system test

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,3 +1,60 @@
 mod bevy_ecs_adapter;
 
-pub use bevy_ecs_adapter::{insert_entity, read_entity, remove_entity, write_entity, Entity, component, system};
+pub use bevy_ecs_adapter::{ECSWorld, ECSSchedule, Entity, component, system};
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::*;
+    use rand::prelude::*;
+
+    #[component]
+    #[derive(Debug, Clone)]
+    struct Position {x: u64, y: u64}
+
+    #[component]
+    #[derive(Clone)]
+    struct Velocity {x: u64, y: u64}
+
+    #[system]
+    fn movement (pos: &mut Position, v: &Velocity, entity: Entity) {
+        let _ = entity;
+        pos.x += v.x;
+        pos.y += v.y;
+    }
+
+    #[test]
+    fn test_simple_calculation_system () {
+        let mut world = ECSWorld::new();
+        let mut entity_storage = HashMap::new();
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..100 {
+            let entity_comps = (
+                Position{x: rng.next_u32() as u64, y: rng.next_u32() as u64},
+                Velocity{x: rng.next_u32() as u64, y: rng.next_u32() as u64}
+            );
+
+            entity_storage.insert(
+                world.create_entity(entity_comps.clone()).id(),
+                entity_comps
+            );
+        }
+
+        let mut schedule = ECSSchedule::new();
+
+        schedule.add_system(movement);
+
+        schedule.run(&mut world);
+
+        entity_storage
+            .iter()
+            .map(|(entity_id, old_entity)| (world.get_entity(*entity_id).unwrap(), old_entity))
+            .for_each(|(new_entity, old_entity)| {
+                let pos = new_entity.get::<Position>().unwrap();
+
+                assert_eq!(pos.x, old_entity.0.x + old_entity.1.x);
+                assert_eq!(pos.y, old_entity.0.y + old_entity.1.y);
+            });
+    }
+}
--- src/ecs/bevy_ecs_adapter.rs
+++ src/ecs/bevy_ecs_adapter.rs
@@ -1,12 +1,56 @@
-pub type Entity = usize;
+use bevy_ecs::bundle::Bundle;
+use bevy_ecs::world::{EntityMut, EntityRef};
 
-pub fn insert_entity() {}
+pub use bevy_ecs::entity::Entity;
+use bevy_ecs::prelude::IntoSystemConfigs;
 
-pub fn read_entity() {}
+pub struct ECSWorld {
+    world: bevy_ecs::world::World
+}
 
-pub fn write_entity() {}
+impl ECSWorld {
+    pub fn new () -> Self {
+        ECSWorld {
+            world: bevy_ecs::world::World::default()
+        }
+    }
 
-pub fn remove_entity() {}
+    pub fn create_entity<B: Bundle>(&mut self, bundle: B) -> EntityMut {
+        self.world.spawn(bundle)
+    }
+
+    pub fn get_entity (&self, entity: Entity) -> Option<EntityRef> {
+        self.world.get_entity(entity)
+    }
+
+    pub fn get_entity_mut (&mut self, entity: Entity) -> Option<EntityMut> {
+        self.world.get_entity_mut(entity)
+    }
+
+    pub fn remove_entity (&mut self, entity: Entity) -> bool {
+        self.world.despawn(entity)
+    }
+}
+
+pub struct ECSSchedule {
+    schedule: bevy_ecs::schedule::Schedule
+}
+
+impl ECSSchedule {
+    pub fn new () -> Self {
+        ECSSchedule {
+            schedule: bevy_ecs::schedule::Schedule::default()
+        }
+    }
+
+    pub fn add_system<M> (&mut self, system: impl IntoSystemConfigs<M>) {
+        self.schedule.add_systems(system);
+    }
+
+    pub fn run (&mut self, world: &mut ECSWorld) {
+        self.schedule.run(&mut world.world);
+    }
+}
 
 pub use animal_game_ecs_macros::to_bevy_component as component;
 pub use animal_game_ecs_macros::to_bevy_system as system;
